2a6241c31dccea24567920d388605c1974712ff0
solar <github@sazabi.org>
2013-05-20 10:32:23 +0900

    add Res<:Response type parameter to IpAddressFilter trait

--- util-finagle-http/src/main/scala/org/sazabi/util/finagle/http/filter/IpAddressFilter.scala
+++ util-finagle-http/src/main/scala/org/sazabi/util/finagle/http/filter/IpAddressFilter.scala
@@ -2,6 +2,7 @@ package org.sazabi.util.finagle.http.filter
 
 import com.twitter.finagle.{Service, SimpleFilter}
 import com.twitter.finagle.http.{Request, Response, Status, Version}
+import com.twitter.finagle.core.util.InetAddressUtil
 import com.twitter.util.{Future, NetUtil}
 
 import java.net.InetAddress
@@ -14,24 +15,26 @@ import syntax.std.option._
  * from allowed ip blocks.
  * This trait uses X-Forwarded-For header if it exists.
  */
-trait IpAddressFilter[Req <: Request] extends SimpleFilter[Req, Response] {
+trait IpAddressFilter[Req <: Request, Res <: Response]
+    extends SimpleFilter[Req, Res] {
   def allowedIpBlocks: Seq[(Int, Int)]
+  def forbidden: Future[Res]
 
   protected def remoteAddress(request: Req): InetAddress = {
-    val xForwardedFor = request.xForwardedFor map { InetAddress.getByName(_) }
+    val xForwardedFor = request.xForwardedFor map(InetAddressUtil.getByName(_))
     xForwardedFor | request.remoteAddress
   }
 
   def isAllowed(a: Int): Boolean = NetUtil.isIpInBlocks(a, allowedIpBlocks)
- 
+
   /**
    * Filters a request.
    */
-  def apply(request: Req, service: Service[Req, Response]): Future[Response] = {
+  def apply(request: Req, service: Service[Req, Res]): Future[Res] = {
     if (isAllowed(NetUtil.inetAddressToInt(remoteAddress(request)))) {
       service(request)
     } else {
-      Future.value(Response(Version.Http11, Status.Forbidden))
+      forbidden
     }
   }
 }
@@ -43,18 +46,22 @@ object IpAddressFilter {
   /**
    * Generates a IPAddressFilter that allows specified ip blocks.
    */
-  def allow[Req <: Request](blocks: Seq[(Int, Int)]): IpAddressFilter[Req] =
-      new IpAddressFilter[Req] {
-    val allowedIpBlocks = blocks
-  }
+  def allow[Req <: Request, Res <: Response](blocks: Seq[(Int, Int)],
+      fbdn: => Future[Res]): IpAddressFilter[Req, Res] =
+    new IpAddressFilter[Req, Res] {
+      val allowedIpBlocks = blocks
+      def forbidden = fbdn
+    }
 
   /**
    * Generates a IPAddressFilter that allows local or specified ip blocks.
    */
-  def allowLocal[Req <: Request](blocks: (Int, Int)*): IpAddressFilter[Req] =
-    new IpAddressFilter[Req] {
-      val allowedIpBlocks = localIpBlocks ++ blocks.toSeq
-    }
+  def allowLocal[Req <: Request, Res <: Response](blocks: (Int, Int)*)(
+    fbdn: => Future[Res]): IpAddressFilter[Req, Res] =
+      new IpAddressFilter[Req, Res] {
+        val allowedIpBlocks = localIpBlocks ++ blocks.toSeq
+        def forbidden = fbdn
+      }
 
   /**
    * Ip address blocks for local.
