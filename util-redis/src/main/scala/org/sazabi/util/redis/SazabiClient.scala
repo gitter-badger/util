f529b43923a4d9cfe3989050d2c84c2937f5b498
solar <github@sazabi.org>
2013-06-05 12:02:00 +0900

    implement new SET syntax introduced in redis 2.6.12

--- util-redis/src/main/scala/org/sazabi/util/redis/SazabiClient.scala
+++ util-redis/src/main/scala/org/sazabi/util/redis/SazabiClient.scala
@@ -28,10 +28,54 @@ class SazabiClient(sf: ServiceFactory[Command, Reply])
       }
     }
   }
-  
+
   def ping(): Future[Unit] = {
     doRequest(Ping) {
       case StatusReply(_) => Future.Unit
+    }
+  }
+
+  def newSet(key: ChannelBuffer, value: ChannelBuffer):
+    SazabiClient.NewSetCommand = SazabiClient.NewSetCommand(this, key, value)
+
+  def setExNx(key: ChannelBuffer, seconds: Long, value: ChannelBuffer):
+    Future[Boolean] = newSet(key, value).ex(seconds).nx()()
+
+  def setPxNx(key: ChannelBuffer, millis: Long, value: ChannelBuffer):
+    Future[Boolean] = newSet(key, value).px(millis).nx()()
+
+  def setXx(key: ChannelBuffer, value: ChannelBuffer): Future[Boolean] =
+    newSet(key, value).xx()()
+
+  def setExXx(key: ChannelBuffer, seconds: Long, value: ChannelBuffer):
+    Future[Boolean] = newSet(key, value).ex(seconds).xx()()
+
+  def setPxXx(key: ChannelBuffer, millis: Long, value: ChannelBuffer):
+    Future[Boolean] = newSet(key, value).px(millis).xx()()
+}
+
+object SazabiClient {
+  case class NewSetCommand(
+      client: Client,
+      key: ChannelBuffer,
+      value: ChannelBuffer,
+      seconds: Option[Long] = None,
+      millis: Option[Long] = None,
+      nxFlag: Boolean = false,
+      xxFlag: Boolean = false) {
+    def ex(seconds: Long) = copy(seconds = Some(seconds))
+
+    def px(millis: Long) = copy(millis = Some(millis))
+
+    def nx() = copy(nxFlag = true)
+
+    def xx() = copy(xxFlag = true)
+
+    def apply(): Future[Boolean] = {
+      client.doRequest(NewSet(key, value, seconds, millis, nxFlag, xxFlag)) {
+        case StatusReply(_) => Future.value(true)
+        case NilMBulkReply() => Future.value(false)
+      }
     }
   }
 }
--- util-redis/src/main/scala/org/sazabi/util/redis/protocol/Command.scala
+++ util-redis/src/main/scala/org/sazabi/util/redis/protocol/Command.scala
@@ -3,6 +3,7 @@ package com.twitter.finagle.redis.protocol
 import com.twitter.finagle.redis.util.StringToChannelBuffer
 
 object SazabiCommands {
+  val SET = "SET"
   val PEXPIRE = "PEXPIRE"
   val PEXPIREAT = "PEXPIREAT"
   val PTTL = "PTTL"
@@ -10,6 +11,7 @@ object SazabiCommands {
 }
 
 object SazabiCommandBytes {
+  val SET = StringToChannelBuffer("SET")
   val PEXPIRE = StringToChannelBuffer("PEXPIRE")
   val PEXPIREAT = StringToChannelBuffer("PEXPIREAT")
   val PTTL = StringToChannelBuffer("PTTL")
